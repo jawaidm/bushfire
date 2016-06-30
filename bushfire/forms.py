089fdd9f21603685fa37184854c681d4c574e082
Jawaid Mushtaq <jawaidm@dpaw.wa.gov.au>
2016-06-30 12:52:22 +0800

    Added an overview tabe for the common fields

--- bushfire/forms.py
+++ bushfire/forms.py
@@ -6,37 +6,35 @@ from bushfire.models import (Bushfire, Activity, Response, AreaBurnt, GroundForc
 from datetime import datetime, timedelta
 from django.conf import settings
 from django.forms import ValidationError
-from django.forms.models import inlineformset_factory, modelformset_factory
+from django.forms.models import inlineformset_factory
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, Div, HTML
 from crispy_forms.bootstrap import TabHolder, Tab
 
 
-class ActivityForm(forms.ModelForm):
+class _ActivityForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         #import ipdb; ipdb.set_trace()
         super(ActivityForm, self).__init__(*args, **kwargs)
 
-        self.helper = FormHelper()
-        self.helper.form_tag = False
+#        self.helper = FormHelper()
 ##        self.helper.form_method = 'post'
-
-        self.helper.layout = Layout(
-            Div(
-                Div('activity',css_class='col-sm-4',),
-                Div('date',css_class='col-sm-2',),
-                css_class='row activity-formset',
-            ),
-        )
+#
+#        self.helper.layout = Layout(
+#            Div(
+#                Div('activity',css_class='col-sm-2',),
+#                Div('date',css_class='col-sm-2',),
+#                css_class='row',
+#            ),
+#        )
 
     class Meta:
         model = Activity
-        # fields = ('activity', 'date',)
-        exclude = ()
+        fields = ('activity', 'date',)
 
 
-class BushfireForm(forms.ModelForm):
+class _BushfireForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         #import ipdb; ipdb.set_trace()
         if kwargs.has_key('user'):
@@ -44,8 +42,7 @@ class BushfireForm(forms.ModelForm):
         super(BushfireForm, self).__init__(*args, **kwargs)
 
         self.helper = FormHelper()
-        self.helper.form_tag = False
-        #self.helper.form_method = 'post'
+        self.helper.form_method = 'post'
 #        self.helper.form_class = 'form-horizontal'
 #        self.helper.label_class = 'col-sm-2'
 #        self.helper.field_class = 'col-sm-8'
@@ -67,23 +64,16 @@ class BushfireForm(forms.ModelForm):
                 Div('authorised_date',css_class='col-sm-2',),
                 css_class='row',
             ),
-            HTML('<hr>'),
-            Div(
-                Div('source',css_class='col-sm-3',),
-                Div('cause',css_class='col-sm-3',),
-                Div('arson_squad_notified',css_class='col-sm-2',),
-                Div('prescription',css_class='col-sm-2',),
-                Div('offence_no',css_class='col-sm-2',),
-                css_class='row',
-            ),
-            HTML('<hr>'),
 
 
             TabHolder(
-#                self.add_origin(),
-#                self.add_location(),
-                self.add_activities(),
-                self.add_comments(),
+                self.add_origin(),
+                self.add_location(),
+                #self.add_activities(),
+                Tab(
+                    'PlaceHolder',
+                    #'location2',
+                ),
 #                Tab(
 #                    'Location',
 #                    Div(
@@ -99,11 +89,11 @@ class BushfireForm(forms.ModelForm):
 #                        css_class='row',
 #                    ),
 #                ),
-#            ),
+            ),
 
 #            ButtonHolder(
 #                Submit('submit', 'Submit', css_class='button white')
-            )
+#            )
 
         )
 
@@ -168,56 +158,40 @@ class BushfireForm(forms.ModelForm):
     def add_activities(self):
         return Tab(
             'Activities',
-            HTML('<p>My Activities</p>'),
             Div(
                 Div('activity',css_class='col-sm-2',),
                 Div('date',css_class='col-sm-2',),
             ),
         )
-
-    def add_comments(self):
-        return Tab(
-            'Comment',
-            HTML('<p>My Comments</p>'),
-            Div(
-                Div('comment',css_class='col-sm-6',),
-            ),
-        )
-
     class Meta:
         model = Bushfire
-        fields = ('region', 'district', 'incident_no', 'season', 'job_code',
-                  'name', 'dfes_incident_no', 'potential_fire_level', 'authorised_by', 'authorised_date',
-                  'source','cause', 'arson_squad_notified', 'prescription', 'offence_no',
-#                  'distance', 'direction', 'place', 'lot_no', 'street', 'town',
-#                  'coord_type', 'fire_not_found', 'lat_decimal', 'lat_degrees', 'lat_minutes',
-#                  'lon_decimal', 'lon_degrees', 'lon_minutes', 'mga_zone', 'mga_easting', 'mga_northing',
-#                  'fd_letter', 'fd_number', 'fd_tenths',
-#                  'fuel','ros', 'flame_height', 'assistance_required', 'fire_contained', 'containment_time',
-#                  'ops_point', 'communications', 'weather', 'field_officer', 'init_authorised_by', 'init_authorised_date',
-                 )
-#        exclude = ()
-
-
-class _BushfireForm(forms.ModelForm):
-    class Meta:
-        model = Bushfire
+        #fields = ('region', 'district', 'incident_no', 'season', 'job_code',)
         fields = ('region', 'district', 'incident_no', 'season', 'job_code',
                   'name', 'dfes_incident_no', 'potential_fire_level', 'authorised_by', 'authorised_date',
                   'distance', 'direction', 'place', 'lot_no', 'street', 'town',
                   'coord_type', 'fire_not_found', 'lat_decimal', 'lat_degrees', 'lat_minutes',
                   'lon_decimal', 'lon_degrees', 'lon_minutes', 'mga_zone', 'mga_easting', 'mga_northing',
                   'fd_letter', 'fd_number', 'fd_tenths',
+                 )
+
+
+class BushfireForm(forms.ModelForm):
+    class Meta:
+        model = Bushfire
+#        fields = ('region', 'district', 'incident_no', 'season', 'job_code',
+#                  'name', 'dfes_incident_no', 'potential_fire_level', 'authorised_by', 'authorised_date',
+#                  'distance', 'direction', 'place', 'lot_no', 'street', 'town',
+#                  'coord_type', 'fire_not_found', 'lat_decimal', 'lat_degrees', 'lat_minutes',
+#                  'lon_decimal', 'lon_degrees', 'lon_minutes', 'mga_zone', 'mga_easting', 'mga_northing',
+#                  'fd_letter', 'fd_number', 'fd_tenths',
 #                  'source','cause', 'arson_squad_notified', 'prescription', 'offence_no',
 #                  'fuel','ros', 'flame_height', 'assistance_required', 'fire_contained', 'containment_time',
 #                  'ops_point', 'communications', 'weather', 'field_officer', 'init_authorised_by', 'init_authorised_date',
-                 )
-#        exclude = ()
+#                 )
+        exclude = ()
 
 
-#ActivityFormSet             = inlineformset_factory(Bushfire, Activity, extra=1, max_num=7, can_delete=True)
-ActivityFormSet             = inlineformset_factory(Bushfire, Activity, form=ActivityForm, extra=1, max_num=7, can_delete=True)
-#ActivityFormSet             = modelformset_factory(Activity, form=ActivityForm, extra=1, max_num=7, can_delete=True)
+ActivityFormSet             = inlineformset_factory(Bushfire, Activity, extra=1, max_num=7, can_delete=True)
 ResponseFormSet             = inlineformset_factory(Bushfire, Response, extra=1, max_num=13, can_delete=True)
 AreaBurntFormSet            = inlineformset_factory(Bushfire, AreaBurnt, extra=1, can_delete=True)
 GroundForcesFormSet         = inlineformset_factory(Bushfire, GroundForces, extra=1, max_num=3, can_delete=True)
